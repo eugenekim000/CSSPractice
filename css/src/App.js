3cf0047c3f55dd039fb09b72dee088cc70656315
eugenekim000 <eugenekim000@gmail.com>
2020-05-23 18:17:34 -0700

    second button

--- css/src/App.js
+++ css/src/App.js
@@ -4,7 +4,22 @@ import './App.css';
 function App() {
 	return (
 		<div className='App'>
-			<button className='button-one'>Hover Me</button>
+			<div className='buttons'>
+				<button className='button-one'>Hover Me</button>
+				<button className='button-two'>Hover Me</button>
+			</div>
+			<div className='text'>
+				<ul>
+					<li>M</li>
+					<li>r</li>
+					<li>.</li>
+					<li>S</li>
+					<li>t</li>
+          <li>a</li>
+          <li>r</li>
+          <li>k</li>
+				</ul>
+			</div>
 		</div>
 	);
 }
